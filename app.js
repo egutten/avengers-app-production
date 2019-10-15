87d5ca1bd9af1fb8b143560c802bab67ff240af9
egutten <guttenberg.emily@gmail.com>
2019-10-15 11:46:05 -0700

    x

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var express          = require("express"),
 
 require('dotenv').config();
 
+console.log("THIS IS A NEW VERSION");
+
 // Requiring routes
 var quoteRoutes      = require("./routes/quotes"),
 	movieRoutes      = require("./routes/movies"),
