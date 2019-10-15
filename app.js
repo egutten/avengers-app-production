09618c9a3261d4c61e64356b70a08d30a1fc493f
egutten <guttenberg.emily@gmail.com>
2019-10-15 11:58:29 -0700

    x

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var quoteRoutes      = require("./routes/quotes"),
 	movieRoutes      = require("./routes/movies"),
 	indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://egutten:peachy6m@ds159184.mlab.com:59184/heroku_4w2fzchw", {useNewUrlParser: true});
+//"mongodb://egutten:peachy6m@ds159184.mlab.com:59184/heroku_4w2fzchw"
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({extended: true}));
